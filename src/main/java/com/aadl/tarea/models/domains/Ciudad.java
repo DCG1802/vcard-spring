24c5a3d46fb95b7face82b04e051f5d82c57fac2
DCG1802 <davidcg20123@gmail.com>
2021-11-11 13:30:38 -0500

    Empiezo
    Solo agregué unos cuantos atributos nos vemos a las 4

--- src/main/java/com/aadl/tarea/models/domains/Ciudad.java
+++ src/main/java/com/aadl/tarea/models/domains/Ciudad.java
@@ -2,4 +2,20 @@ package com.aadl.tarea.models.domains;
 
 public class Ciudad {
 
+	private Integer codigoCiudad;
+	private String nombreCiudad;
+	
+	
+	public Integer getCodigoCiudad() {
+		return codigoCiudad;
+	}
+	public void setCodigoCiudad(Integer codigoCiudad) {
+		this.codigoCiudad = codigoCiudad;
+	}
+	public String getNombreCiudad() {
+		return nombreCiudad;
+	}
+	public void setNombreCiudad(String nombreCiudad) {
+		this.nombreCiudad = nombreCiudad;
+	}
 }
--- src/main/java/com/aadl/tarea/models/domains/Empleado.java
+++ src/main/java/com/aadl/tarea/models/domains/Empleado.java
@@ -2,4 +2,8 @@ package com.aadl.tarea.models.domains;
 
 public class Empleado {
 
+	private String nombres;
+	private String apellidos;
+	private Ciudad ciudad;
+	
 }
