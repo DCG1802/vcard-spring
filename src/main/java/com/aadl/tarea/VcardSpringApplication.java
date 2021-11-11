695da35cc7f7bc86b9a99339f14ad85918f4d871
Alejo30 <alejocoraizaca@gmail.com>
2021-11-11 10:51:36 -0500

    Commit Inicial
    Commit Inicial

---
+++ src/main/java/com/aadl/tarea/VcardSpringApplication.java
@@ -0,0 +1,13 @@
+package com.aadl.tarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcardSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcardSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aadl/tarea/VcardSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aadl.tarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcardSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
