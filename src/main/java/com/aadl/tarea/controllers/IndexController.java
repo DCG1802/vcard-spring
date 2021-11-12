e150f214c2c2d4dbb6f5b5d3352abc0e4606b8f0
DCG1802 <davidcg20123@gmail.com>
2021-11-12 13:32:38 -0500

    Prueba
    Solo mapee para probar unas cosas, nada relevante por el momento

--- src/main/java/com/aadl/tarea/controllers/IndexController.java
+++ src/main/java/com/aadl/tarea/controllers/IndexController.java
@@ -1,5 +1,11 @@
 package com.aadl.tarea.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class IndexController {
 
+	@RequestMapping("/ver")
+	public String index() {
+		return "index";
+	}
 }
